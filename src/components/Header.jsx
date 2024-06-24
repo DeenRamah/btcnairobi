c0967d9570425a9b8d816a8c371b3f1db2f31cf0
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-06-24 23:51:09 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -54,7 +54,7 @@ const Header = () => {
       }`}
     >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
-        <a className="block w-[12rem] xl:mr-8 " href="https://web3-five-wine.vercel.app">
+        <a className="block w-[12rem] xl:mr-8 " href="#hero">
           <img
           src={brainwave}
           width={100}
